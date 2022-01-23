c36b8db28689c46a3c7973c197a8dc606d3de47d
msterpa87 <sterpa.1152419@studenti.uniroma1.it>
2022-01-23 16:26:28 +0100

    fixed minor issue

--- dataloader.py
+++ dataloader.py
@@ -5,6 +5,7 @@ from networkx.convert_matrix import to_scipy_sparse_matrix
 from collections import defaultdict
 from spektral.data import Dataset
 from spektral.data.graph import Graph
+from contextlib import suppress
 
 from utils import *
 
@@ -28,9 +29,12 @@ class WICO(Dataset):
             pathname = f"{self.custom_path}/{graph_type}/"
             subgraphs_list = list(map(int, filter(str.isnumeric, listdir(pathname))))
 
-            for graph_id in sorted(subgraphs_list)[:50]:
+            for graph_id in sorted(subgraphs_list):
                 G = load_graph_from_file(f"{pathname}/{graph_id}",
                                         root_edges=True, time_delay_edges=True)
+                
+                if G is None:
+                    continue
 
                 A = to_scipy_sparse_matrix(G, dtype=float)
 
@@ -44,8 +48,11 @@ class WICO(Dataset):
                 tuples = [x[1:] for x in df.itertuples() if x[1] in G.nodes]
 
                 for i, (user_id, tweet_time, friends, followers) in enumerate(tuples):
+                    friends, followers = (0, 0)
 
-                    friends, followers = user_info[user_id]
+                    with suppress(KeyError):
+                        friends, followers = user_info[user_id]
+                    
                     n_tweets = user_tweet_count[user_id]
 
                     x[i] = np.array([friends, followers, n_tweets, tweet_time], dtype=float)
